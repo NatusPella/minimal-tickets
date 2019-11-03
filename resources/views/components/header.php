fab1d3b27ae338db31b45057db42a80dccbeb008
Alexander Buist <a.buist@natuspella.nl>
2019-11-03 13:13:02 +0100

    Changed the navbar

--- resources/views/components/header.php
+++ resources/views/components/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <!-- Bootstrap CSS -->
+  <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Fontawesome -->
@@ -25,12 +25,39 @@
               <?php
               if (Core::isAuthenticated()) {
               ?>
-                <li class="nav-item"><a href='?action=createTicket' class="nav-link">Create</a></li>
-                <li class="nav-item"><a href='?action=ownerList' class="nav-link">My tickets</a></li>
-                <li class="nav-item"><a href='?action=handlerList' class="nav-link">My work</a></li>
+                <li class="nav-item"><a href='?action=index' class="nav-link"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-ticket-alt"></i> Tickets
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href='?action=createTicket'><i class="fas fa-plus"></i> Create</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href='?action=handlerList'><i class="fas fa-inbox"></i> My work</a>
+
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href='?action=ownerList'><i class="fas fa-user"></i> My tickets</a>
+                    <!--<li class="nav-item"><a href='?action=createTicket' class="nav-link"><i class="fas fa-plus"></i> Create</a></li>
+                    <li class="nav-item"><a href='?action=ownerList' class="nav-link"><i class="fas fa-user"></i> My tickets</a></li>
+                    <li class="nav-item"><a href='?action=handlerList' class="nav-link"><i class="fas fa-inbox"></i> My work</a></li>-->
+                  </div>
+                </li>
               </ul>
               <ul class="navbar-nav">
-                <li class="nav-item"><a href='?action=logout' class="nav-link">Logout</a></li>
+
+                <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                   <i class="fas fa-user"></i> <?= $_SESSION['currentUser']->email ?>
+                 </a>
+                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                   <a class="dropdown-item" href="?action=profile"><i class="fas fa-user"></i> My profile</a>
+                   <div class="dropdown-divider"></div>
+                   <a class="dropdown-item" href="?action=logout"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                 </div>
+                </li>
+                <!--<li class="nav-item"><a href='?action=profile' class="nav-link"><?= $_SESSION['currentUser']->name ?></a></li>
+                <li class="nav-item"><a href='?action=logout' class="nav-link">Logout</a></li>-->
               <?php
               } else {
               ?>
