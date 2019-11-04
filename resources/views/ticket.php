50fb19a2e1c63a2017d584108a3329ef2cda817b
Alexander Buist <a.buist@natuspella.nl>
2019-11-04 10:43:33 +0100

    Changed the layout of the page

--- resources/views/ticket.php
+++ resources/views/ticket.php
@@ -8,23 +8,26 @@ require_once("resources/views/components/header.php");
     <div class="col">
       <h2 title="ID: <?= $data['ticket']->id ?>"> <?= $data['ticket']->title ?></h2>
 
-
       <p>
-      <?php
-      foreach ($data['ticket']->tags as $tagId => $tagValue) {
-        echo "<span class='badge badge-light'><i class='fas fa-tag'></i> $tagValue</span>";
-      }
+        <span class='badge badge-light'><i class="far fa-circle"></i> <?= ucfirst($data['ticket']->status) ?></span>
 
-      ?>
+        <?php
+        foreach ($data['ticket']->tags as $tagId => $tagValue) {
+          echo "<span class='badge badge-light'><i class='fas fa-tag'></i> $tagValue</span>";
+        }
+        ?>
 
+        <span class='badge badge-light'><i class="fas fa-user"></i> <?= User::getById($data['ticket']->owner)->name ?> (Owner)</span>
+        <span class='badge badge-light'><i class="fas fa-user-tag"></i> <?= User::getById($data['ticket']->handler)->name ?> (Handler)</span>
+        <span class='badge badge-light'><i class="fas fa-calendar-plus"></i> <?= date("Y-m-d H:i:s", $data['ticket']->updated) ?></span>
+        <span class='badge badge-light'><i class="fas fa-calendar"></i> <?= date("Y-m-d H:i:s", $data['ticket']->created) ?></span>
       </p>
-      <p><span class="badge badge-secondary"><?= ucfirst($data['ticket']->status) ?></span> <i class="fas fa-user"></i> <?= User::getById($data['ticket']->owner)->name ?> <i class="fas fa-user-tag"></i> <?= User::getById($data['ticket']->handler)->name ?> <i class="fas fa-calendar-plus"></i> <?= date("Y-m-d H:i:s", $data['ticket']->updated) ?> <i class="fas fa-calendar"></i> <?= date("Y-m-d H:i:s", $data['ticket']->created) ?></p>
 
       <?php
       foreach ($data['ticket']->comments as $comment) {
       ?>
       <p>
-        <b><?= User::getById($comment->owner)->name ?></b> - <?= date("Y-m-d H:i:s", $comment->created) ?><br>
+        <b><i class="fas fa-user"></i> <?= User::getById($comment->owner)->name ?></b> <i class="fas fa-calendar"></i> <?= date("Y-m-d H:i:s", $comment->created) ?><br>
         <?= $comment->content ?>
       </p>
       <?php
